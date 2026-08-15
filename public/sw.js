efc3722cce5b51af34a0b9b057a1f68ae3b68218
Tanveer Siraj <sameershah6594@gmail.com>
2026-08-16 00:27:16 +0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,29 @@
+// Combined Monetag Service Worker (sw.js)
+
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11564163
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+    "zoneId": 11584744
+};
+self.lary = "";
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
+
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11584761
+};
+self.lary = "";
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
+
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11584748
+};
+self.lary = "";
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
+
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11584752
+};
+self.lary = "";
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
